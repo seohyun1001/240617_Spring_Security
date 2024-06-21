d70110bc5bdae256606935373626916d28be1358
seohyun1001 <sh54991210@gmail.com>
2024-06-21 15:29:29 +0900

    15:30 / UserController 수정 - 롤 변경

--- src/main/java/com/example/shopbackend/controller/UserController.java
+++ src/main/java/com/example/shopbackend/controller/UserController.java
@@ -1,40 +1,54 @@
 package com.example.shopbackend.controller;
 
-import com.example.shopbackend.model.User;
+import com.example.shopbackend.model.Role;
+import com.example.shopbackend.security.UserPrinciple;
 import com.example.shopbackend.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
 public class UserController {
-    @Autowired
-    private UserService userService;
 
-    @GetMapping("/home")
-    public String Home() {
-        return "Hello World";
+    private final UserService userService;
+
+    @PutMapping("/change/{role}")
+    public ResponseEntity<Object> changeRole(@AuthenticationPrincipal UserPrinciple userPrinciple,
+                                             @PathVariable Role role) {
+        userService.changeRole(role, userPrinciple.getUsername());
+        return ResponseEntity.ok(true);
     }
 
-    @PostMapping("/user")
-    public User newUser(@RequestBody User newUser) {
-        return userService.saveUser(newUser);
-    }
+//    @GetMapping("/home")
+//    public String Home() {
+//        return "Hello World";
+//    }
+//
+//    @PostMapping("/user")
+//    public User newUser(@RequestBody User newUser) {
+//        return userService.saveUser(newUser);
+//    }
+//
+//    @GetMapping("/users")
+//    public List<User> findAllUsers() {
+//        return userService.findAll();
+//    }
+//
+//    @GetMapping("/user/{username}")
+//    public Optional<User> getUserByUsername(@PathVariable String username) {
+//        return userService.findByUsername(username);
+//    }
+//
+//    @PutMapping("user")
+//    public void changeRole(@RequestBody User user){
+//        userService.changeRole(user.getRole(), user.getUsername());
+//    }
 
-    @GetMapping("/users")
-    public List<User> findAllUsers() {
-        return userService.findAll();
-    }
 
-    @GetMapping("/user/{username}")
-    public Optional<User> getUserByUsername(@PathVariable String username) {
-        return userService.findByUsername(username);
-    }
-
-    @PutMapping("user")
-    public void changeRole(@RequestBody User user){
-        userService.changeRole(user.getRole(), user.getUsername());
-    }
 }
