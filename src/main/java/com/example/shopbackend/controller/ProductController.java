92f2c296e2ba467c30e841132339a5a65ea2b182
seohyun1001 <sh54991210@gmail.com>
2024-06-21 15:30:12 +0900

    15:30 / ProductController 추가 - 제품 조회, 추가, 삭제

---
+++ src/main/java/com/example/shopbackend/controller/ProductController.java
@@ -0,0 +1,33 @@
+package com.example.shopbackend.controller;
+
+import com.example.shopbackend.model.Product;
+import com.example.shopbackend.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<Object> saveProduct(@RequestBody Product product) {
+        return new ResponseEntity<>(productService.saveProduct(product), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getAllProducts() {
+        return new ResponseEntity<>(productService.findAllProducts(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("{productId}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable Long productId) {
+        productService.deleteProduct(productId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
